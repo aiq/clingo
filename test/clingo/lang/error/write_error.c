51486ea555786ac251bd873272752ebf4a805949
aiq <alex.schulz.mail@googlemail.com>
2022-07-09 22:21:37 +0200

    add write_error_c test

---
+++ test/clingo/lang/error/write_error.c
@@ -0,0 +1,31 @@
+#include "clingo/io/FILE.h"
+#include "clingo/lang/error.h"
+#include "clingo/lang/expect.h"
+
+#define expect_recorded_( Rec, Exp )                                           \
+{                                                                              \
+   expect_c_(                                                                  \
+      recorded_is_c( (Rec), (Exp) )                                            \
+   );                                                                          \
+   reset_recorder_c( Rec );                                                    \
+}
+
+int main( void )
+{
+   init_tap_c_();
+
+   cErrorStack* es = &error_stack_c_( 248 );
+   cRecorder* rec = &recorder_c_( 1024 );
+
+   push_error_c_( es, &C_Eof );
+   push_lit_str_error_c( es, "file error" );
+   push_decode_error_c( es, "some type" );
+   write_error_c( rec, es->err, "" );
+   expect_recorded_( rec, "not able to decode 'some type': file error: EOF" );
+
+   push_encode_error_c( es, "other type" );
+   write_error_c( rec, es->err, "" );
+   expect_recorded_( rec, "not able to encode 'other type': not able to decode 'some type': file error: EOF" );
+
+   return finish_tap_c_();
+}
