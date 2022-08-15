64832bffc3564fe3863c8fcb5ba17e231e56a792
aiq <alex.schulz.mail@googlemail.com>
2022-08-16 00:21:05 +0200

    switch to target specific flags

--- mk/cmake/Custom.cmake
+++ mk/cmake/Custom.cmake
@@ -4,19 +4,19 @@
 
 set( CMAKE_COLOR_MAKEFILE ON )
 
-set( CUSTOM_FLAGS_LIST
-   -Wall
-   -Wabsolute-value
-   -Werror=absolute-value
-   -Waddress-of-temporary
-   -Werror=address-of-temporary
-   -Wconversion # Warn for implicit conversions that may alter a value.
-   -Werror=conversion
-   -Wunused-function
-   -Werror=unused-function
-)
-list( JOIN CUSTOM_FLAGS_LIST " " CUSTOM_FLAGS )
-set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CUSTOM_FLAGS}" )
+function( set_default_flags TargetName )
+   target_compile_options( ${TargetName} PRIVATE
+      -Wall
+      -Wabsolute-value
+      -Werror=absolute-value
+      -Waddress-of-temporary
+      -Werror=address-of-temporary
+      -Wconversion # Warn for implicit conversions that may alter a value.
+      -Werror=conversion
+      -Wunused-function
+      -Werror=unused-function
+   )
+endfunction( set_default_flags )
 
 ################################################################################
 # custom functions
