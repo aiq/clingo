3a4f6aed1ed1cd07b9e4e4240c1c497b468316ee
aiq <alex.schulz.mail@googlemail.com>
2022-10-01 10:02:44 +0200

    renamed temporarly bsearch to _BSEARCH

---
+++ test/clingo/lang/algo/_REMOVE.c
@@ -0,0 +1,26 @@
+#include "clingo/lang/expect.h"
+#include "clingo/type/cChars.h" // remove_char_c
+
+#define expect_( Deck, Cstr )                                                  \
+   expect_c_(                                                                  \
+      chars_is_c( as_c_( cChars, (Deck) ), (Cstr) )                            \
+   )
+
+int main( void )
+{
+   init_tap_c_();
+
+   cVarChars slice = slice_c_( char, 'a', 'b', 'c', 'd', 'e' );
+   expect_( slice, "abcde" );
+
+   remove_char_c( &slice, 2 );
+   expect_( slice, "abde" );
+
+   remove_char_c( &slice, 0 );
+   expect_( slice, "bde" );
+
+   remove_char_c( &slice, 2 );
+   expect_( slice, "bd" );
+
+   return finish_tap_c_();
+}
