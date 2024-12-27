8d316005250733c238c59e942a7059f2b67413e5
aiq <aiq@posteo.de>
2024-12-27 16:10:38 +0100

    refactor: better tests for set_on_bit_vec

--- test/clingo/container/CBitVec/set_on_bit_vec.c
+++ test/clingo/container/CBitVec/set_on_bit_vec.c
@@ -1,29 +1,50 @@
 #include "clingo/container/CBitVec.h"
 #include "clingo/lang/expect.h"
 
+TEMP_SLICE_C_(
+   test,
+   {
+      char const* inp;
+      cInt64s positions;
+      cByte val;
+      char const* exp;
+   }
+)
+#define t_( ... ) ((test){__VA_ARGS__})
+
 int main( void )
 {
    init_tap_c_();
 
-   CBitVec* vec = new_bit_vec_c( 15 );
+   testSlice tests = slice_c_( test,
+      t_(
+         .inp = "0000 0000 0000 000",
+         .positions = (cInt64s)slice_c_( int64_t, 0, 3, 4, 7, 10, 11, 14 ),
+         .val = 1,
+         .exp = "1001 1001 0011 001"
+      ),
+      t_(
+         .inp = "1001 1001 0011 001",
+         .positions = (cInt64s)slice_c_( int64_t, 19 ),
+         .val = 1,
+         .exp = "1001 1001 0011 0010 0001"
+      )
+   );
 
-   expect_at_c_( bit_vec_is_c_( vec, "0000 0000 0000 000" ) );
-
-   set_on_bit_vec_c( vec, 0, 1 );
-   set_on_bit_vec_c( vec, 3, 1 );
-   set_on_bit_vec_c( vec, 4, 1 );
-   set_on_bit_vec_c( vec, 7, 1 );
-   set_on_bit_vec_c( vec, 10, 1 );
-   set_on_bit_vec_c( vec, 11, 1 );
-   set_on_bit_vec_c( vec, 14, 1 );
-
-   expect_at_c_( bit_vec_is_c_( vec, "1001 1001 0011 001" ) );
-
-   set_on_bit_vec_c( vec, 19, 1 );
-
-   expect_at_c_( bit_vec_is_c_( vec, "1001 1001 0011 0010 0001" ) );
-   
-   release_c( vec );
+   for_each_c_( i, test const*, t, tests )
+   {
+      CBitVec* vec = bit_vec_from_cstr_c( t->inp );
+      each_c_( int64_t const*, pos, t->positions )
+      {
+         set_on_bit_vec_c( vec, *pos, t->val );
+      }
+      expect_block_c_( i, bit_vec_is_c_( vec, t->exp ) )
+      {
+         tap_exp_line_c_( "{s}", t->exp );
+         tap_got_line_c_( "{t:01/80/4}", bit_vec_tape_c( vec ) );
+      }
+      release_c( vec );
+   }
 
    return finish_tap_c_();
 }
